ba7dce24e059e241c262583e118ef12ee7e188f6
Yoosters Cobite <yoosters.cobite@gmail.com>
2017-06-30 11:28:51 -0400

    work on  comment

---
+++ app/controllers/comments.rb
@@ -0,0 +1,20 @@
+get '/comments/:id' do 
+  @question = Question.find(params[:id])
+  @comments = @question.comments.all
+
+  erb :'comments/show'
+end
+
+get '/comments/new' do
+ 
+  erb :'comments/new'
+end
+
+post '/comments' do
+  @comment = Comment.new(params[:comment])
+  if @comment.save
+    redirect '/comments'
+  else
+    erb :'comments/new'
+  end
+end
