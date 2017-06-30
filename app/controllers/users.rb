380aa479713842137f31fda3fad2857d82f53f2a
Adam Weil & Evans Wang & Raja & yoosters cobite <pair+adamjweil.evansjwang.raja-sharma.yoosters@devbootcamp.com>
2017-06-30 11:37:10 -0400

    user auth

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,12 +6,22 @@ get '/register' do
   erb :'user/register'
 end
 
-post '/login' do
+post '/register' do
   create_user
   redirect '/questions'
 end
 
 
-get '/register' do
-  erb :'user/register'
+post '/sessions' do
+  @user = User.find_by(email: params[:user_information][:email])
+  if @user && @user.password == params[:user_information][:password]
+    session[:user_id] = @user.id
+    redirect '/questions'
+  else
+    "Login Attempt Failed"
+  end
+end
+
+get '/logout' do
+  @user = nil
 end
