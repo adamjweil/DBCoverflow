1b8deeeafb642b68eaeefd170cb65aa527f3d649
Evans Wang <evansjwang@gmail.com>
2017-06-30 12:20:01 -0400

    Added AJAX to delete and upvote, need to correct downvote

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,16 +3,32 @@ get '/questions' do
   erb :'/questions/questions'
 end
 
+delete '/questions/:id' do
+    if request.xhr?
+      Question.destroy(params[:id])
+    end
+    redirect '/questions'
+end
+
 post '/questions/:id/upvote' do
 #  if logged_in && (User.find_by(id: session[:id]).votes.all.find_by(id: params[:id]).votes == 1.abs)
 #  could be different for upvote and downvote
-  Vote.create(votes: 1, user_id: User.find_by(id: session[:id]).id, votable_id: Question.find_by(id: params[:id]).id, votable_type: "Question")
-  redirect '/questions'
+  Vote.create(votes: 1, user_id: User.find_by(id: 1).id, votable_id: Question.find_by(id: params[:id]).id, votable_type: "Question")
+  question = Question.find_by(id: params[:id])
+  if request.xhr?
+    erb :'/questions/_totalvotes', layout: false, locals: {question: question}
+  else
+    redirect '/questions'
+  end
 end
 
 post '/questions/:id/downvote' do
   Vote.create(votes: -1, user_id: User.find_by(id: session[:id]).id, votable_id: Question.find_by(id: params[:id]).id, votable_type: "Question")
-  redirect '/questions'
+  if request.xhr?
+    erb :'/questions/_totalvotes', layout: false, locals: {question: question}
+  else
+    redirect '/questions'
+  end
 end
 
 post '/comments/:id/upvote' do
