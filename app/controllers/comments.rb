19605ade3a6a69fadf831a58a2e77c959bc93d86
Raja & yoosters cobite <pair+raja-sharma.yoosters@devbootcamp.com>
2017-06-30 16:15:06 -0400

    forms and comment/answer routes

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,19 +1,20 @@
-get '/comments/:id' do
-  @question = Question.find(params[:id])
-  @comments = @question.comments.all
-
-  erb :'comments/show'
-end
-
-get '/comments/new' do
-  erb :'comments/new'
-end
-
-post '/comments' do
-  @comment = Comment.new(params[:comment])
+post '/questions/:id/comments' do
+  @comment = Comment.new(comments: params[:comments], commentable_type: params[:commentable_type], commentable_id: params[:id])
   if @comment.save
-    redirect '/comments'
+    redirect "/answers/#{params[:id]}"
   else
     erb :'comments/new'
   end
 end
+
+
+get '/questions/:id/comments/new' do
+  erb :'comments/new'
+end
+
+# get '/questions/:id/comments/:id' do
+#   @question = Question.find(params[:id])
+#   @comments = @question.comments.all
+#
+#   erb :'comments/show'
+# end
